35f196aa438c4fdeb91229a9248ebf68e389016a
Mark <ywtean@gmail.com>
2016-06-06 14:55:46 +0800

    Initial commit

---
+++ app/src/main/java/com/marakana/android/yamba/SettingsActivity.java
@@ -0,0 +1,22 @@
+package com.marakana.android.yamba;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SettingsActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		// Check if this activity was created before
+		if (savedInstanceState == null) {
+			// Create a fragment
+			SettingsFragment fragment = new SettingsFragment();
+			getFragmentManager()
+					.beginTransaction()
+					.add(android.R.id.content, fragment,
+							fragment.getClass().getSimpleName()).commit();
+		}
+	};
+}
